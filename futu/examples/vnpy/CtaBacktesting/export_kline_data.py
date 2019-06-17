635e14ad9c89b60d1d49bdb3e1f53fcd77237786
FUTUNN\ysq <ysq@vip.qq.com>
2019-06-17 11:11:33 +0800

    tiger api test

--- futu/examples/vnpy/CtaBacktesting/export_kline_data.py
+++ futu/examples/vnpy/CtaBacktesting/export_kline_data.py
@@ -2,6 +2,10 @@ from futu import *
 from time import sleep
 import datetime
 import pandas as pd
+from tigeropen.common.consts import *
+from tigeropen.tiger_open_config import TigerOpenClientConfig
+from tigeropen.common.util.signature_utils import read_private_key
+from tigeropen.quote.quote_client import QuoteClient
 
 EXPORT_STOCK_CODE = 'HK.00700'
 HOST = '127.0.0.1'
@@ -58,6 +62,65 @@ def export_stock_kline(stock_code, host, port, kltype=KLType.K_1M, file_path=Non
     quote_ctx.close()
 
 
+def get_tiger_client_config(sandbox=False):
+    """
+    https://www.itiger.com/openapi/info 开发者信息获取
+    :return:
+    """
+    client_config = TigerOpenClientConfig(sandbox_debug=sandbox)
+    client_config.private_key = read_private_key('c:/rsa_private_key.pem')  # read_private_key('填写私钥文件的路径')
+    client_config.tiger_id = '20150277'
+    client_config.account = 'U10192539'
+    client_config.standard_account = None
+    client_config.paper_account = '20190424102103233'  # 模拟账户
+    client_config.language = Language.zh_CN
+    return client_config
+
+
+import datetime as dt
+
+
+def export_tiger_kline(kltype=BarPeriod.DAY):
+    client_config = get_tiger_client_config()
+    quote_client = QuoteClient(client_config)
+
+    stock = ['00700']
+    init_year = 2000
+    limit_bars = 1000
+    for year in range(init_year, 2020, 1):
+        start = dt.datetime(year, 1, 1, 0, 0, 0)
+        end = dt.datetime(year, 12, 31, 23, 59, 59)
+        bars = quote_client.get_bars(stock, period=kltype, begin_time=int(start.timestamp()) * 1000, end_time=int(end.timestamp()) * 1000, limit=limit_bars)
+        # sleep(1)
+        if bars is not None and len(bars):
+            end = int(bars.iloc[0:1]['time'])
+            print(bars)
+            print(end)
+            print(year)
+            d1 = dt.datetime.now().timestamp()
+            dt_start = d1
+            req_count = 1
+            try:
+                while True:
+                    bars = quote_client.get_bars(stock, period=kltype, begin_time=int(start.timestamp()) * 1000,
+                                                 end_time=end, limit=limit_bars)
+                    req_count += 1
+                    d2 = dt.datetime.now().timestamp()
+                    dt_sec = d2 - d1
+                    d1 = d2
+                    print('time:{}'.format(dt_sec))
+                    print('-----------------------------------------------')
+                    sleep(0.2)
+            except Exception as e:
+                rate_speed = req_count / (d1 - dt_start)
+                print(e)
+                print('req count: {}, speed rate: {}/s'.format(req_count, rate_speed))
+
+            print(bars)
+            break
+
 if __name__ == "__main__":
     # export_stock_kline(EXPORT_STOCK_CODE, HOST, PORT, KLType.K_1M)
-    export_stock_kline('US.AAPL', HOST, PORT, KLType.K_1M)
+    # export_stock_kline('US.AAPL', HOST, PORT, KLType.K_1M)
+
+    export_tiger_kline(BarPeriod.ONE_MINUTE)
